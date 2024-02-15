2d5b5ca861b7fb4d0b7d8082d98d778b34c2dca6
Guillemj98 <guillemagro98@gmail.com>
2024-02-15 13:54:46 +0100

    He añadido 3 metodos nuevos

--- _13_ClasesObjetosArraysGrupal/src/_13_ClasesObjetosArraysGrupal/Usuario.java
+++ _13_ClasesObjetosArraysGrupal/src/_13_ClasesObjetosArraysGrupal/Usuario.java
@@ -47,6 +47,35 @@ public class Usuario {
 	public void setValoraciones(int[] valoraciones) {
 		this.valoraciones = valoraciones;
 	}
+	public double devolverValoracionMedia() {
+		double media = 0;
+		double suma = 0;
+		for(int i = 0; i<this.valoraciones.length;i++) {
+			suma += this.valoraciones[i];
+		}
+		media = suma/this.valoraciones.length;
+		return media;
+		
+	}
+	public void motrarValoraciones() {
+		
+		for(int i = 0;i < this.valoraciones.length ;i++) {
+			System.out.println("Lista de valoraciones");
+			System.out.println("Valoracion  " + i + " : " +  this.valoraciones[i]);
+			
+		}
+	}
+	
+	public int devolverPuntuacionesMasAltas(double nota) {
+		int notas_mayores = 0;
+		for (double d : valoraciones) {
+			if (d>nota) {
+				notas_mayores++;
+			}
+
+		}
+		return notas_mayores;
+	}
 	
 	
 	
